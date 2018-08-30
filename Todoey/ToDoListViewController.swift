f9a1c9c93319881b33e38805b8b620495baf0619
Amarr <azerig@unistra.fr>
2018-08-30 15:57:54 +0200

    Ajout des checkmarques Ajout de l'animation lors du clic

---
+++ Todoey/ToDoListViewController.swift
@@ -0,0 +1,62 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by AmarTest on 28/08/2018.
+//  Copyright © 2018 Amar. All rights reserved.
+//
+
+import UIKit
+
+class ToDoListViewController: UITableViewController {
+
+    let itemArray = ["Find Mike", "Buy Eggos", "Destory Demogorgon"]
+   
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        
+        
+        cell.textLabel?.text = itemArray[indexPath.row]
+    
+       
+        
+        return cell
+    }
+
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+            
+            print(itemArray[indexPath.row])
+            
+        } else {
+            
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+            
+            print(itemArray[indexPath.row])
+        }
+        
+        
+        
+    }
+    
+
+}
+
